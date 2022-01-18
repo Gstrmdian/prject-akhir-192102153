7cb1db572dd732b9692922dfe514104d61add311
insp1r4t <dandiokta91@gmail.com>
2022-01-18 15:50:24 +0700

    commit uas

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppMenuComponent } from './app-menu/app-menu.component';
+import { DivisionComponent } from './division/division.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { TambahDataDivisiComponent } from './tambah-data-divisi/tambah-data-divisi.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppHeaderComponent,
+    AppMenuComponent,
+    DivisionComponent,
+    DashboardComponent,
+    AppFooterComponent,
+    TambahDataDivisiComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/division/division.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Division } from '../model/division.model';
+import { DivisionService } from '../services/division.service';
+
+
+@Component({
+  selector: 'app-division',
+  templateUrl: './division.component.html',
+  styleUrls: ['./division.component.css']
+})
+export class DivisionComponent implements OnInit {
+
+  division?:Division[];
+  currentDivision:Division={};
+  name='';
+
+  constructor(private divisionService:DivisionService) { }
+
+  ngOnInit(): void {
+    this.retrieveDivision()
+  }
+
+  retrieveDivision():void{
+    this.divisionService.getAll()
+      .subscribe(
+        data=>{
+          this.division=data;
+          console.log(data);
+        },
+        error=>{
+          console.log(error)
+        }
+      )
+  }
+
+}
